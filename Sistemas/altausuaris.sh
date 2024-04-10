f1419bd6ad23a2dad31199e1585719eefbf9a0f1
Cuenta12345 <cuentadosxxi@gmail.com>
2024-04-10 18:42:14 +0200

    Mejora de script

--- Sistemas/altausuaris.sh
+++ Sistemas/altausuaris.sh
@@ -2,29 +2,41 @@
 
 if [ $# -eq 2 ]; then 
 	echo -e "Se han recibido los ficheros\n"
-
-	#Se guardan los dni's
 	echo -e "Recuperación de datos de usuarios del fichero [fitxer_prova_usuaris]"
-	dnis=$(cat fitxer_prova_usuaris | tail -n +2 | awk -F ":" '{print $1}')
-	apellidos=$(cat fitxer_prova_usuaris | tail -n +2 | awk -F ":" '{print $2}' | awk -F "," '{print $1}')
-	nombres=$(cat fitxer_prova_usuaris | tail -n +2 | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
-	telefonos=$(cat fitxer_prova_usuaris | tail -n +2 | awk -F ":" '{print $3}')
-	proyectos=$(cat fitxer_prova_usuaris | tail -n +2 | awk -F ":" '{print $4}')
+	tail -n +2 "$1" | while IFS= read -r linea; do
+    		dnis=$(echo "$linea" | awk -F ":" '{print $1}')
+	        apellidos=$(echo "$linea" | awk -F ":" '{print $2}' | awk -F "," '{print $1}')
+        	nombres=$(echo "$linea" | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $2); print $2}')
+        	telefonos=$(echo "$linea" | awk -F ":" '{print $3}')
+        	proyectos=$(echo "$linea" | awk -F ":" '{print $4}')
 
-	echo -e "\nDNIs: $dnis\n"
-        echo -e "\napellidos: $apellidos\n"
-        echo -e "\nnombres: $nombres\n"
-        echo -e "\ntelefonos: $telefonos\n"
-        echo -e "\nproyectos: $proyectos\n"
+        	echo -e "\nDNIs: $dnis"
+        	echo -e "\napellidos: $apellidos"
+        	echo -e "\nnombres: $nombres"
+        	echo -e "\ntelefonos: $telefonos"
+        	echo -e "\nproyectos: $proyectos"
+                echo -e "\n----------------------------------------------------------------\n"
+	done
 
+	echo -e "________________________________________________________________________________"
 	echo -e "Recuperación de datos de proyectos del fichero [fitxer_prova_projectes]"
+        tail -n +2 "$2" | while IFS= read -r linea; do
+		proyectos=$(echo "$linea" | awk -F ":" '{print $1}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
+                jefes=$(echo "$linea" | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
+                descripciones=$(echo "$linea" | awk -F ":" '{print $3}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
+
+	        echo -e "\nNombres de proyectos: $proyectos"
+       		echo -e "\nJefes de proyectos: $jefes"
+        	echo -e "\nDescripciones de proyectos: $descripciones"
+		echo -e "\n----------------------------------------------------------------\n"
+	done
 	proyectos=$(cat fitxer_prova_projectes | tail -n +2 | awk -F ":" '{print $1}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
 	caps=$(cat fitxer_prova_projectes | tail -n +2 | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
 	descripciones=$(cat fitxer_prova_projectes | tail -n +2 | awk -F ":" '{print $3}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
 
-	echo -e "\nNombres de proyectos: \n$proyectos\n"
-        echo -e "\nJefes de proyectos: \n$caps\n"
-        echo -e "\nDescripciones de proyectos: \n$descripciones\n"
+#	echo -e "\nNombres de proyectos: \n$proyectos\n"
+#        echo -e "\nJefes de proyectos: \n$caps\n"
+#        echo -e "\nDescripciones de proyectos: \n$descripciones\n"
 
 else
 	echo "No se ha recibido nada"
