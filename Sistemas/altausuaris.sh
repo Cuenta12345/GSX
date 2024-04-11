01c86134b5a2631557addc7ee01647f9b5cc7ff7
Cuenta12345 <cuentadosxxi@gmail.com>
2024-04-11 18:41:21 +0200

    Empezando configuracion permisos directorios

--- Sistemas/altausuaris.sh
+++ Sistemas/altausuaris.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #Primero se crean los directorios necesarios para tenerlos preparados
 
-mkdir -p /empresa/usuaris /empresa/projectes /empresa/bin 
+mkdir -p /empresa/usuaris /empresa/projectes /empresa/bin
 
 #Ahora se comprueba que se sea un usuario privilegiado para ejecutar el script
 
@@ -13,14 +13,14 @@ fi
 
 #Se comprueba que se hayan pasado dos ficheros
 
-if [ $# -eq 2 ]; then 
+if [ $# -eq 2 ]; then
 	echo -e "Se han recibido los ficheros\n"
 
 	#Se realiza lo sigueinte:
 	#-Se recupera y crean los usuarios
 	#-Se crean los grupos
 	#-Se introducen los usuarios en los respectivos grupos
-	
+
 	echo -e "Recuperación de datos de usuarios del fichero [fitxer_prova_usuaris]"
 	tail -n +2 "$1" | while IFS= read -r linea; do
     		dnis=$(echo "$linea" | awk -F ":" '{print $1}')
@@ -28,15 +28,15 @@ if [ $# -eq 2 ]; then
         	nombres=$(echo "$linea" | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $2); print $2}')
         	telefonos=$(echo "$linea" | awk -F ":" '{print $3}')
         	proyectos=$(echo "$linea" | awk -F ":" '{print $4}')
-		
+
 		#Se prepara el nombre del usuario como nombre + Iniciales del los apellidos
 
 		iniciales=$(echo "$apellidos" | awk -F " " '{for (i=1; i<=NF; i++) printf substr($i,1,1)}' | tr '[:lower:]' '[:upper:]')
 		nombre_aux=$(echo "$nombres" | tr -d ' ')
 		usuario=$(echo "${nombre_aux}${iniciales}")
-		
+
 		#Primeros preparamos los grupos
-	
+
 		if  ! id "$usuario" &>/dev/null; then
 			echo "El usuaio $usuario no existe"
 			echo "[Creandolo]"
@@ -49,41 +49,38 @@ if [ $# -eq 2 ]; then
                         if ! grep -q "$proyecto" /etc/group 2>/dev/null; then
                                 groupadd "$proyecto"
                         fi
-                        usermod -aG $proyecto $usuario 
+                        usermod -aG $proyecto $usuario
                 done
 
 
-        	#echo -e "\nDNIs: $dnis"
-        	#echo -e "\napellidos: $apellidos"
-        	#echo -e "\nnombres: $nombres"
-	       	#echo -e "\ntelefonos: $telefonos"
-        	#echo -e "\nproyectos: $proyectos"
-                #echo -e "\n----------------------------------------------------------------\n"
+        	echo -e "\nDNIs: $dnis"
 		id $usuario
 	done
 
 	echo -e "________________________________________________________________________________"
 	echo -e "Recuperación de datos de proyectos del fichero [fitxer_prova_projectes]"
         tail -n +2 "$2" | while IFS= read -r linea; do
-		proyectos=$(echo "$linea" | awk -F ":" '{print $1}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
-                jefes=$(echo "$linea" | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
-                descripciones=$(echo "$linea" | awk -F ":" '{print $3}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
+								proyecto=$(echo "$linea" | awk -F ":" '{print $1}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
+                DNIjefe=$(echo "$linea" | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
+                descripcione=$(echo "$linea" | awk -F ":" '{print $3}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
+								#Crear direcotrios
+								echo "Creando directorios para el proyecto: $proyecto"
+								mkdir -p /empresa/projectes/$proyecto
 
-	        #echo -e "\nNombres de proyectos: $proyectos"
-       		#echo -e "\nJefes de proyectos: $jefes"
-        	#echo -e "\nDescripciones de proyectos: $descripciones"
-		#echo -e "\n----------------------------------------------------------------\n"
+								#Añado el propietario, el grupo, y los permisos
+								apellidos=$(cat fitxer_prova_usuaris | grep "$DNIjefe" | awk -F ":" '{print $2}' | awk -F "," '{print $1}' | awk -F " " '{for (i=1; i<=NF; i++) printf substr($i,1,1)}' | tr '[:lower:]' '[:upper:]')
+			        	nombres=$(cat fitxer_prova_usuaris | grep "$DNIjefe" | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $2); print $2}' | tr -d ' ')
+								propietario=$(echo "${nombres}${apellidos}")
+								chown $propietario:$proyecto /empresa/projectes/$proyecto
+								chmod 2770 /empresa/projectes/$proyecto
+								chmod g+s /empresa/projectes/$proyecto
+								echo "umask 0002" >> /etc/profile.d/$proyecto.sh
+								echo "jefe: $DNIjefe | Propietario: $propietario"
 	done
-	proyectos=$(cat fitxer_prova_projectes | tail -n +2 | awk -F ":" '{print $1}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
-	caps=$(cat fitxer_prova_projectes | tail -n +2 | awk -F ":" '{print $2}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
-	descripciones=$(cat fitxer_prova_projectes | tail -n +2 | awk -F ":" '{print $3}' | awk -F "," '{sub(/^ */, "", $1); print $1}')
 
-#	echo -e "\nNombres de proyectos: \n$proyectos\n"
-#        echo -e "\nJefes de proyectos: \n$caps\n"
-#        echo -e "\nDescripciones de proyectos: \n$descripciones\n"
+	chmod 1775 /empresa/bin
+
 
 else
 	echo "No se ha recibido nada"
 fi
-
-
