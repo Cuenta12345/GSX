42ddd87d99041b846f489868434e343991285c1a
Cuenta12345 <cuentadosxxi@gmail.com>
2024-04-18 20:12:35 +0200

    treballproj modificado

--- Sistemas/treballproj.sh
+++ Sistemas/treballproj.sh
@@ -6,10 +6,6 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-# Nombre del proyecto
-actual_dir=$(pwd)
-new_directorio="/empresa/projectes/$1"
-old_umask=$(umask)
 cd /empresa/projectes/$1
 umask 007
 newgrp $1
