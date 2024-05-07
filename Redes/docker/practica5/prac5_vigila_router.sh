66057be8c6b40322f62b877119bc7ea65745d53f
Cuenta12345 <cuentadosxxi@gmail.com>
2024-05-07 21:42:58 +0200

    Finalizada practica 5

--- Redes/docker/practica5/prac5_vigila_router.sh
+++ Redes/docker/practica5/prac5_vigila_router.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-(crontab -l ; echo "*/5 * * * * /root/prac5/vigila_snmp.sh") | crontab -
+(crontab -l ; echo "*/5 * * * * ./root/prac5/vigila_snmp.sh") | crontab -
 echo "Cron configurado para ejecutar /root/vigila_snmp.sh cada 5 minutos."
 service cron start
--- Redes/docker/practica5/proves_snmp_remotes.sh
+++ Redes/docker/practica5/proves_snmp_remotes.sh
@@ -1,25 +1,37 @@
 #! /bin/bash
-echo "-------------Primeras pruebas-------------" > sortida_snmp_remota_prac5.txt
+echo -e "\n-------------Primeras pruebas-------------" > sortida_snmp_remota_prac5.txt
 snmptranslate -Td -OS UCD-SNMP-MIB::ucdavis.dskTable >> sortida_snmp_remota_prac5.txt
 
-echo "-------------Primeras pruebas en Remoto-------------" >> sortida_snmp_remota_prac5.txt
-echo "-------------Prueba de system-------------" >> sortida_snmp_remota_prac5.txt
+echo -e "\n-------------Primeras pruebas en Remoto-------------" >> sortida_snmp_remota_prac5.txt
+echo -e "\n-------------Prueba de system-------------" >> sortida_snmp_remota_prac5.txt
 snmpwalk -v 2c -c public 198.18.248.1 system >> sortida_snmp_remota_prac5.txt
 
-echo "-------------Prueba de hrSystem-------------" >> sortida_snmp_remota_prac5.txt
+echo -e "\n-------------Prueba de system V3 gsxViewer-------------" >> sortida_snmp_remota_prac5.txt 
+snmpwalk -v3 -u gsxViewer -l authNoPriv -a SHA -A aut84205317 198.18.248.1 system >> sortida_snmp_remota_prac5.txt
+
+echo -e "\n-------------Prueba de system V3 gsxAdmin-------------" >> sortida_snmp_remota_prac5.txt 
+snmpwalk -v3 -u gsxAdmin -l authPriv -a SHA -A aut84205317 -x DES -X sec84205317 198.18.248.1 system >> sortida_snmp_remota_prac5.txt
+
+echo -e "\n-------------Prueba de hrSystem-------------" >> sortida_snmp_remota_prac5.txt
 snmpwalk -v 2c -c public 198.18.248.1 hrSystem >> sortida_snmp_remota_prac5.txt
 
-echo "-------------Pruebas acceso mibs de la Univ Remotas con gsxViewer-------------" >> sortida_snmp_remota_prac5.txt
+echo -e "\n-------------Prueba de hrSystem V3 gsxViewer-------------" >> sortida_snmp_remota_prac5.txt 
+snmpwalk -v3 -u gsxViewer -l authNoPriv -a SHA -A aut84205317 198.18.248.1 hrSystem >> sortida_snmp_remota_prac5.txt
+
+echo -e "\n-------------Prueba de hrSystem V3 gsxAdmin-------------" >> sortida_snmp_remota_prac5.txt 
+snmpwalk -v3 -u gsxAdmin -l authPriv -a SHA -A aut84205317 -x DES -X sec84205317 198.18.248.1 hrSystem >> sortida_snmp_remota_prac5.txt
+
+echo -e "\n-------------Pruebas acceso mibs de la Univ Remotas con gsxViewer-------------" >> sortida_snmp_remota_prac5.txt
 snmptable -v3 -u gsxViewer -l authNoPriv -a SHA -A aut84205317 198.18.248.1 UCD-SNMP-MIB::prTable >> sortida_snmp_remota_prac5.txt
 snmptable -v3 -u gsxViewer -l authNoPriv -a SHA -A aut84205317 198.18.248.1 ucdavis.dskTable >> sortida_snmp_remota_prac5.txt
 snmptable -v3 -u gsxViewer -l authNoPriv -a SHA -A aut84205317 198.18.248.1 ucdavis.laTable >> sortida_snmp_remota_prac5.txt
 
-echo "-------------Pruebas acceso mibs de la Univ Remotas con gsxAdmin-------------" >> sortida_snmp_remota_prac5.txt
+echo -e "\n-------------Pruebas acceso mibs de la Univ Remotas con gsxAdmin-------------" >> sortida_snmp_remota_prac5.txt
 snmptable -v3 -u gsxAdmin -l authPriv -a SHA -A aut84205317 -x DES -X sec84205317 198.18.248.1 UCD-SNMP-MIB::prTable >> sortida_snmp_remota_prac5.txt
 snmptable -v3 -u gsxAdmin -l authPriv -a SHA -A aut84205317 -x DES -X sec84205317 198.18.248.1 ucdavis.dskTable >> sortida_snmp_remota_prac5.txt
 snmptable -v3 -u gsxAdmin -l authPriv -a SHA -A aut84205317 -x DES -X sec84205317 198.18.248.1 ucdavis.laTable >> sortida_snmp_remota_prac5.txt
 
-echo "-------------Prueba Remota con gsxViewer-------------" >> sortida_snmp_remota_prac5.txt
+echo -e "\n-------------Prueba Remota con gsxViewer-------------" >> sortida_snmp_remota_prac5.txt
 snmpget -v3 -u gsxViewer -l authNoPriv -a SHA -A aut84205317 198.18.248.1 system.sysDescr.0 >> sortida_snmp_remota_prac5.txt
-echo "-------------Prueba Remota con gsxAdmin--------------" >> sortida_snmp_remota_prac5.txt
+echo -e "\n-------------Prueba Remota con gsxAdmin--------------" >> sortida_snmp_remota_prac5.txt
 snmpget -v3 -u gsxAdmin -l authPriv -a SHA -A aut84205317 -x DES -X sec84205317 198.18.248.1 system.sysDescr.0 >> sortida_snmp_remota_prac5.txt
